91cda1c00b4ecb9afa90a371ad80607c98ef6563
pr-field <knnokpet@gmail.com>
2014-04-28 12:08:12 +0900

    GapedTweet を表示するセルを作成。

---
+++ MBTwitter/MBGapedTweetViewCell.h
@@ -0,0 +1,15 @@
+//
+//  MBGapedTweetViewCell.h
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/04/25.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBGapedTweetViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIButton *gapButton;
+@property (nonatomic) NSInteger index;
+
+@end
---
+++ MBTwitter/MBGapedTweetViewCell.m
@@ -0,0 +1,34 @@
+//
+//  MBGapedTweetViewCell.m
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/04/25.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import "MBGapedTweetViewCell.h"
+
+@implementation MBGapedTweetViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
