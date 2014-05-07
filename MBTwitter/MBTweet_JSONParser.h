8578064ae49d2656991f07f688b4b97cd6ab0eff
pr-field <knnokpet@gmail.com>
2014-05-07 22:21:17 +0900

    MBTweet_JSONParser を作成。

---
+++ MBTwitter/MBTweet_JSONParser.h
@@ -0,0 +1,13 @@
+//
+//  MBTweet_JSONParser.h
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/05/05.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import "MBJSONParser.h"
+
+@interface MBTweet_JSONParser : MBJSONParser
+
+@end
---
+++ MBTwitter/MBTweet_JSONParser.m
@@ -0,0 +1,24 @@
+//
+//  MBTweet_JSONParser.m
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/05/05.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import "MBTweet_JSONParser.h"
+
+@implementation MBTweet_JSONParser
+
+- (void)configure:(id)parsedObj
+{
+    if ([parsedObj isKindOfClass:[NSDictionary class]]) {
+        MBTweet *tweet = [[MBTweet alloc] initWithDictionary:(NSDictionary *)parsedObj];
+        [[MBTweetManager sharedInstance] storeTweet:tweet];
+        [[MBUserManager sharedInstance] storeUser:tweet.tweetUser];
+        NSArray *gotTweet = [NSArray arrayWithObject:tweet];
+        self.completion(gotTweet);
+    }
+}
+
+@end
