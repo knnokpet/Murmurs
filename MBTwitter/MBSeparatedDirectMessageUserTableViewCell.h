05af430c08ddc58d9422c32e113d3e6b50d64da2
pr-field <knnokpet@gmail.com>
2014-05-07 22:23:40 +0900

    MBSeparatedDirectMessageUserTableViewCell を作成。

---
+++ MBTwitter/MBSeparatedDirectMessageUserTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  MBSeparatedDirectMessageUserTableViewCell.h
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/04/29.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBSeparatedDirectMessageUserTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *iconImageView;
+@property (weak, nonatomic) IBOutlet UILabel *screenNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *characterNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *subtitleLabel;
+
+@end
