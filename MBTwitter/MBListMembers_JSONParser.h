78842aaef368c4970de761f5dbd5400d55cd9b83
pr-field <knnokpet@gmail.com>
2014-05-09 20:07:15 +0900

    ListMembers_JSONParser を追加。

---
+++ MBTwitter/MBListMembers_JSONParser.h
@@ -0,0 +1,13 @@
+//
+//  MBListMembers_JSONParser.h
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/05/09.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import "MBJSONParser.h"
+
+@interface MBListMembers_JSONParser : MBJSONParser
+
+@end
