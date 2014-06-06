00ef6e593d88fc622cc455782bf5ddbae81d3afd
pr-field <knnokpet@gmail.com>
2014-06-06 16:28:35 +0900

    画像のリサイズ、角丸化を担当するクラスを作成。

---
+++ MBTwitter/MBImageApplyer.h
@@ -0,0 +1,16 @@
+//
+//  MBImageApplyer.h
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/06/01.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBImageApplyer : NSObject
+
++ (UIImage *)imageForTwitter:(UIImage *)image byScallingToFillSize:(CGSize)size radius:(CGFloat)radius;
++ (UIImage *)imageForTwitter:(UIImage *)image byScallingAspectFillSize:(CGSize)size radius:(CGFloat)radius;
+
+@end
