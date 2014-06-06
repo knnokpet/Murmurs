64d388dfa5fff6800466f3d03704bcbec4ad45d1
pr-field <knnokpet@gmail.com>
2014-06-06 15:59:24 +0900

    リストの編集のためのクラスを作成。

---
+++ MBTwitter/MBTextFieldTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  MBTextFieldTableViewCell.h
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/05/15.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBTextFieldTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+@end
---
+++ MBTwitter/MBTextFieldTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  MBTextFieldTableViewCell.m
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/05/15.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import "MBTextFieldTableViewCell.h"
+
+@implementation MBTextFieldTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
