0995ffd02dd2c5dcd224f7e11161016c6bcb3885
pr-field <knnokpet@gmail.com>
2014-06-06 15:59:41 +0900

    リストの編集のためのクラスを作成。

---
+++ MBTwitter/MBSwitchTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  MBSwitchTableViewCell.h
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/05/15.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBSwitchTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UISwitch *switchView;
+
+@end
---
+++ MBTwitter/MBSwitchTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  MBSwitchTableViewCell.m
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/05/15.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import "MBSwitchTableViewCell.h"
+
+@implementation MBSwitchTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
