8a6eb3b9335b3dba15e9d069892426e3709d23fe
pr-field <knnokpet@gmail.com>
2014-06-06 16:00:21 +0900

    リストを編集するためのセルクラスの作成。

---
+++ MBTwitter/MBEditListUserTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MBEditListUserTableViewCell.h
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/06/05.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBEditListUserTableViewCell : UITableViewCell
+
+@end
---
+++ MBTwitter/MBEditListUserTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  MBEditListUserTableViewCell.m
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/06/05.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import "MBEditListUserTableViewCell.h"
+
+@implementation MBEditListUserTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
