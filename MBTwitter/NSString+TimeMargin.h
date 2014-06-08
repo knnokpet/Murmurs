4aae30fbf8982a77d98ccc4d7887d2ecc95912bb
pr-field <knnokpet@gmail.com>
2014-06-08 18:32:40 +0900

    タイムラインの TimeInterval を取得するためのカテゴリを追加。

---
+++ MBTwitter/NSString+TimeMargin.h
@@ -0,0 +1,15 @@
+//
+//  NSString+TimeMargin.h
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/06/07.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (TimeMargin)
+
++ (NSString *)timeMarginWithDate:(NSDate *)date;
+
+@end
