66797bb298b47e82bf997915a092dd08c82d7e8a
pr-field <knnokpet@gmail.com>
2014-07-03 17:26:25 +0900

    listSegmentedControlView クラスを作成。

---
+++ MBTwitter/MBListSegmentedContainerView.h
@@ -0,0 +1,13 @@
+//
+//  MBListSegmentedContainerView.h
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/06/28.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBListSegmentedContainerView : UIView
+
+@end
