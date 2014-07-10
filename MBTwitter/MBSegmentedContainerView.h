9587957670f6679f9ab8bd0eb6bd04f863621603
pr-field <knnokpet@gmail.com>
2014-07-11 01:29:00 +0900

    SegmentedControl のコンテナとしてのクラスを作成。

---
+++ MBTwitter/MBSegmentedContainerView.h
@@ -0,0 +1,13 @@
+//
+//  MBSegmentedContainerView.h
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/07/09.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBSegmentedContainerView : UIView
+
+@end
