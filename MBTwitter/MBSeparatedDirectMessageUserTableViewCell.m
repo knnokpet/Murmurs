9f1e476b66de5fa8c8478788163aa1a3da39438d
pr-field <knnokpet@gmail.com>
2014-07-13 00:11:38 +0900

    表示に関するプロパティを追加。
    AvatorImageView, CharacterScreenNameView, に関するプロパティ、セッターを追加。

--- MBTwitter/MBSeparatedDirectMessageUserTableViewCell.m
+++ MBTwitter/MBSeparatedDirectMessageUserTableViewCell.m
@@ -27,11 +27,14 @@
 
 - (void)common
 {
-    self.iconImageView.backgroundColor = [UIColor clearColor];
-    self.iconImageView.layer.masksToBounds = YES;
-    self.iconImageView.layer.cornerRadius = 4.0f;
+    self.avatorImageView.backgroundColor = [UIColor clearColor];
+    self.avatorImageView.layer.masksToBounds = NO;
+    self.avatorImageView.layer.shouldRasterize = YES;
+    self.avatorImageView.layer.cornerRadius = 4.0f;
 }
 
+#pragma mark -
+#pragma mark Setter & Getter
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
     [super setSelected:selected animated:animated];
@@ -39,45 +42,29 @@
     // Configure the view for the selected state
 }
 
-- (void)setScreenName:(NSString *)screenName
+- (void)setDateString:(NSString *)dateString
 {
-    _screenName = screenName;
-    if (0 < screenName.length) {
-        NSString *nameAtmark = [NSString stringWithFormat:@"@%@", screenName];
-        self.screenNameLabel.text = nameAtmark;
-        [self setNeedsDisplay];
-    }
+    _dateString = dateString;
+    
+    self.dateLabel.text = dateString;
 }
 
-- (void)layoutSubviews
+- (void)setCharaScreenString:(NSAttributedString *)charaScreenString
 {
-    [super layoutSubviews];
-    
-    [self.characterNameLabel sizeToFit];
-    [self.screenNameLabel sizeToFit];
-    CGFloat namesSpace = (self.dateLabel.frame.origin.x - self.characterNameLabel.frame.origin.x);
-    if (namesSpace < self.characterNameLabel.frame.size.width + self.screenNameLabel.frame.size.width) {
-        CGRect screenFrame = self.screenNameLabel.frame;
-        CGFloat screenWidth = namesSpace - self.characterNameLabel.frame.size.width;
-        if (screenWidth < 0) {
-            screenWidth = 0.0f;
-        }
-        screenFrame.size.width = screenWidth;
-        self.screenNameLabel.frame = screenFrame;
-    }
-    CGRect screenFrame = self.screenNameLabel.frame;
-    screenFrame.origin.x = self.characterNameLabel.frame.origin.x + self.characterNameLabel.frame.size.width + 8;
-    self.screenNameLabel.frame = screenFrame;
-    
-    if ( namesSpace < self.characterNameLabel.frame.size.width) {
-        CGRect characterFrame = self.characterNameLabel.frame;
-        characterFrame.size.width = (self.dateLabel.frame.origin.x - self.characterNameLabel.frame.origin.x - 8);
-        self.characterNameLabel.frame = characterFrame;
-        
-        CGRect screenFrame = self.screenNameLabel.frame;
-        screenFrame.size.width = 0.f;
-        self.screenNameLabel.frame = screenFrame;
-    }
+    _charaScreenString = charaScreenString;
+    self.characterScreenNameView.characterScreenString = charaScreenString;
+}
+
+- (void)setSubtitleString:(NSString *)subtitleString
+{
+    _subtitleString = subtitleString;
+    self.subtitleLabel.text = subtitleString;
+}
+
+- (void)setUserIDStr:(NSString *)userIDStr
+{
+    _userIDStr = userIDStr;
+    [self.avatorImageView setUserIDStr:userIDStr];
 }
 
 @end
