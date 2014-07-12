0abd67cf331e73dc8cad62aee4879fa9f95f6561
pr-field <knnokpet@gmail.com>
2014-07-13 00:12:14 +0900

    複数ユーザーを表示する際のセルクラスを作成。

---
+++ MBTwitter/MBUsersTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  MBUsersTableViewCell.h
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/07/11.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBUsersTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *avatorImageView;
+@property (weak, nonatomic) IBOutlet UILabel *characterNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *screenNameLabel;
+
+
+@end
---
+++ MBTwitter/MBUsersTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  MBUsersTableViewCell.m
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/07/11.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import "MBUsersTableViewCell.h"
+
+@implementation MBUsersTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+    [self common];
+}
+
+- (void)common
+{
+    self.avatorImageView.backgroundColor = [UIColor clearColor];
+    self.avatorImageView.layer.cornerRadius = 4.0f;
+    self.avatorImageView.layer.masksToBounds = NO;
+    self.avatorImageView.layer.shouldRasterize = YES;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
