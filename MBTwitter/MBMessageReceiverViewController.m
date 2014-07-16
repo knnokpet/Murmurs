632cd4c1f74c44a6ed70682a8b8b04ea3e9deed4
pr-field <knnokpet@gmail.com>
2014-07-16 23:42:20 +0900

    表示の実装。
    MBUserTableViewCell を導入。画像や characterName を表示できるように。

--- MBTwitter/MBMessageReceiverViewController.m
+++ MBTwitter/MBMessageReceiverViewController.m
@@ -11,6 +11,13 @@
 #import "MBUserManager.h"
 #import "MBUser.h"
 #import "MBUserIDManager.h"
+#import "MBImageDownloader.h"
+#import "MBImageCacher.h"
+#import "MBImageApplyer.h"
+
+#import "MBUsersTableViewCell.h"
+
+static NSString *usersCellIdentifier = @"MBUsersTableViewCellIdentifier";
 
 @interface MBMessageReceiverViewController ()
 
@@ -34,6 +41,9 @@
     // Do any additional setup after loading the view from its nib.
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
+    
+    UINib *usersCellNib = [UINib nibWithNibName:@"MBUsersTableViewCell" bundle:nil];
+    [self.tableView registerNib:usersCellNib forCellReuseIdentifier:usersCellIdentifier];
 }
 
 - (void)didReceiveMemoryWarning
@@ -49,6 +59,16 @@
     return 2;
 }
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    CGFloat height = 44.0f;
+    if (indexPath.section == 0) {
+        height = 52;
+    }
+    
+    return height;
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     if (1 == section) {
@@ -61,14 +81,18 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *cellIdentifier = @"CellIdentifier";
-    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
-    }
+    
+    
+    UITableViewCell *cell;
     
     if (0 == indexPath.section) {
-        [self updateCell:cell atIndexPath:indexPath];
+        cell = [self.tableView dequeueReusableCellWithIdentifier:usersCellIdentifier];
+        [self updateCell:(MBUsersTableViewCell *)cell atIndexPath:indexPath];
     } else if (1 == indexPath.section) {
+        cell = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+        if (!cell) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+        }
         [self updateScreenNameCell:cell atIndexpath:indexPath];
     }
     
@@ -76,11 +100,43 @@
     return cell;
 }
 
-- (void)updateCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath
+- (void)updateCell:(MBUsersTableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath
 {
     NSString *key = [self.dataSource objectAtIndex:indexPath.row];
     MBUser *userAtIndex = [[MBUserManager sharedInstance] storedUserForKey:key];
-    cell.textLabel.text = userAtIndex.characterName;
+    cell.characterNameLabel.text = userAtIndex.characterName;
+    cell.screenName = userAtIndex.screenName;
+    cell.textLabel.textColor = [UIColor blackColor];
+    
+    UIImage *avatorImage = [[MBImageCacher sharedInstance] cachedProfileImageForUserID:userAtIndex.userIDStr];
+    if (!avatorImage) {
+        cell.avatorImageView.image = [UIImage imageNamed:@"DefaultImage@2x"];
+        if (NO == userAtIndex.isDefaultProfileImage) {
+            
+            dispatch_queue_t globalQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+            dispatch_async(globalQueue, ^{
+                [MBImageDownloader downloadBigImageWithURL:userAtIndex.urlHTTPSAtProfileImage completionHandler:^(UIImage *image, NSData *imageData){
+                    if (image) {
+                        [[MBImageCacher sharedInstance] storeProfileImage:image data:imageData forUserID:userAtIndex.userIDStr];
+                        CGSize imageSize = CGSizeMake(cell.avatorImageView.frame.size.width, cell.avatorImageView.frame.size.height);
+                        UIImage *radiusImage = [MBImageApplyer imageForTwitter:image byScallingToFillSize:imageSize radius:cell.avatorImageView.layer.cornerRadius];
+                        [[MBImageCacher sharedInstance] storeTimelineImage:radiusImage forUserID:userAtIndex.userIDStr];
+                        
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            cell.avatorImageView.image = radiusImage;
+                        });
+                    }
+                    
+                }failedHandler:^(NSURLResponse *response, NSError *error){
+                    
+                }];
+                
+            });
+            
+        }
+    } else {
+        cell.avatorImageView.image = avatorImage;
+    }
 }
 
 - (void)updateScreenNameCell:(UITableViewCell *)cell atIndexpath:(NSIndexPath *)indexPath
