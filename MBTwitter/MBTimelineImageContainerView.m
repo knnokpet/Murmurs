c71feaf762d2f0852b1375bb53fa2f5bae798107
pr-field <knnokpet@gmail.com>
2014-07-25 21:23:20 +0900

    タイムラインにおける、画像メディアを表示するコンテナビュークラスを作成。

---
+++ MBTwitter/MBTimelineImageContainerView.m
@@ -0,0 +1,52 @@
+//
+//  MBTimelineImageContainerView.m
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/07/25.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import "MBTimelineImageContainerView.h"
+
+@implementation MBTimelineImageContainerView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setImageCount:(NSInteger)imageCount
+{
+    _imageCount = imageCount;
+    [self configureImageView];
+}
+
+- (void)setImages:(NSArray *)images
+{
+    _images = images;
+}
+
+- (void)configureImageView
+{
+    CGSize boundsSize = self.bounds.size;
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, boundsSize.width, boundsSize.height)];
+    imageView.image = [UIImage imageNamed:@"DefaultImage"];
+    [self addSubview:imageView];
+    
+    _imageViews = @[imageView];
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
