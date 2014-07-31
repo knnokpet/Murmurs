7f9ed91172697b6e3d5363ce7481856b84d9454d
pr-field <knnokpet@gmail.com>
2014-07-31 20:31:24 +0900

    角丸な枠線を描くことが出来るボタンクラスの作成。
    未完成。

---
+++ MBTwitter/MBRoundButton.h
@@ -0,0 +1,13 @@
+//
+//  MBRoundButton.h
+//  MBTwitter
+//
+//  Created by Masayuki Ikeda on 2014/07/29.
+//  Copyright (c) 2014年 Masayuki Ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBRoundButton : UIButton
+
+@end
