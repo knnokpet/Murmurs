a38c9b1d175cedeeb2325a07925bed79ea317d3d
pr-field <knnokpet@gmail.com>
2014-08-23 00:27:12 +0900

    ピンの表示を、内部のクラスで描画するのではなく、UIImageView で表す様に。

--- MBTwitter/MBGeoPinView.m
+++ MBTwitter/MBGeoPinView.m
@@ -26,21 +26,76 @@
     return self;
 }
 
+
 - (void)drawRect:(CGRect)rect
 {
-    [self.dotColor set];
-    UIBezierPath *dotPath = [UIBezierPath bezierPathWithOvalInRect:self.bounds];
-    [dotPath fill];
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGColorRef harfAlphaColorRef = CGColorCreateCopyWithAlpha(self.dotColor.CGColor, 0.5f);
+    CGContextSetStrokeColorWithColor(context, harfAlphaColorRef);
+    CGContextSetFillColorWithColor(context, self.dotColor.CGColor);
+    
+    CGContextAddEllipseInRect(context, rect);
+    CGColorRelease(harfAlphaColorRef);
+    
+    CGContextSetFillColorWithColor(context, [UIColor whiteColor].CGColor);
+    CGSize whiteEllipseSize = CGSizeMake(rect.size.width / 4, rect.size.height / 4);
+    CGRect whiteEllipseRect = CGRectMake(rect.origin.x + whiteEllipseSize.width, rect.origin.y + whiteEllipseSize.height, whiteEllipseSize.width, whiteEllipseSize.height);
+    CGContextAddEllipseInRect(context, whiteEllipseRect);
+    
 }
 
 @end
 
 
 
+@interface MBPinView : UIView
+
+@end
+
+
+@implementation MBPinView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor clearColor];
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGFloat r,g,b;
+    r = 200 / 255;
+    g = 200 / 255;
+    b = 200 / 255;
+    CGContextSetRGBStrokeColor(context, r, g, b, 0.5f);
+    CGContextSetRGBFillColor(context, r, g, b, 1.0f);
+    
+    CGContextMoveToPoint(context, rect.origin.x, rect.origin.y);
+    CGContextAddLineToPoint(context, rect.origin.x + rect.size.width, rect.origin.y);
+    CGContextAddLineToPoint(context, rect.origin.x + rect.size.width, rect.origin.y + rect.size.height);
+    CGContextAddLineToPoint(context, rect.origin.x, rect.origin.y + rect.size.height);
+    CGContextAddLineToPoint(context, rect.origin.x, rect.origin.y);
+    CGContextClosePath(context);
+    
+    CGContextDrawPath(context, kCGPathFillStroke);
+}
+
+@end
+
+
+
+
+
 @interface MBGeoPinView()
 
-@property (nonatomic, readonly) MBDotView *dotView;
-@property (nonatomic, readonly) UIView *hogeView;
+@property (nonatomic, readonly) UIImageView *dotView;
+@property (nonatomic, readonly) UIImageView *pinView;
+@property (nonatomic, readonly) UIImageView *pinHoleView;
 
 @end
 
@@ -53,19 +108,31 @@
         // Initialization code
         self.backgroundColor = [UIColor clearColor];
         
-        _dotView = [[MBDotView alloc] initWithFrame:CGRectMake(0, 0, 12, 12)];
-        CGPoint dotPoint = CGPointMake(self.frame.size.width / 2 - self.dotView.frame.size.width / 2, 0);
-        CGRect dotRect = self.dotView.frame;
-        dotRect.origin = dotPoint;
-        self.dotView.frame = dotRect;
+        // Dot
+        CGFloat radius = 6.0f;
+        _dotView = [[UIImageView alloc] initWithFrame:CGRectMake(self.frame.size.width / 2 - radius, 0, radius * 2, radius * 2)];
+        CGFloat r,g,b,a;
+        r = 122 / 255;
+        g = 255 / 255;
+        b = 60 / 255;
+        a = 1.0f;
+        //self.dotView.dotColor = [UIColor colorWithRed:r green:g blue:b alpha:a];
+        self.dotView.image = [UIImage imageNamed:@"Dot-Green"];
         [self addSubview:self.dotView];
         
-        CGFloat hogeWidth = 2.0f;
-        CGFloat hogeOriginY = self.dotView.frame.origin.y + self.dotView.frame.size.height;
-        CGRect hogeRect = CGRectMake(self.frame.size.width / 2 - hogeWidth / 2, hogeOriginY, hogeWidth, self.frame.size.height - hogeOriginY);
-        _hogeView = [[UIView alloc] initWithFrame:hogeRect];
-        self.hogeView.backgroundColor = [UIColor blackColor];
-        [self addSubview:self.hogeView];
+        // Pint
+        CGFloat pinWidth = 1.0f;
+        CGFloat pinOriginY = self.dotView.frame.origin.y + self.dotView.frame.size.height - 1;
+        CGRect pinRect = CGRectMake(self.frame.size.width / 2 - pinWidth / 2, pinOriginY, pinWidth, self.frame.size.height - pinOriginY);
+        _pinView = [[UIImageView alloc] initWithFrame:pinRect];
+        self.pinView.image = [UIImage imageNamed:@"Pin"];
+        [self addSubview:self.pinView];
+        
+        // PinHole
+        CGFloat pinHoleWidth = 2.0f;
+        CGFloat pinHoleHeight = 1.0f;
+        _pinHoleView = [[UIImageView alloc] initWithFrame:CGRectMake(self.frame.size.width / 2 - pinHoleWidth / 2, self.frame.size.height - pinHoleHeight, pinHoleWidth, pinHoleHeight)];
+        self.pinHoleView.image = [UIImage imageNamed:@"PinHole"];
     }
     return self;
 }
@@ -80,8 +147,20 @@
     
 }*/
 
+- (void)showPinHoleView:(BOOL)shows
+{
+    if (shows) {
+        [self addSubview:self.pinHoleView];
+    } else {
+        [self.pinHoleView removeFromSuperview];
+    }
+    
+}
+
 - (void)boundingAnimateDotWithCompletion:(CompletionHandler)completion
 {
+    [self showPinHoleView:YES];
+    
     CGRect endRect = self.dotView.frame;
     CGRect boundingRect = endRect;
     boundingRect.origin.y = boundingRect.origin.y + 4.0f;
@@ -99,22 +178,25 @@
     [UIView animateWithDuration:0.1f animations:^{
         self.dotView.transform = CGAffineTransformMakeScale(0.5, 2.0);
         self.dotView.transform = CGAffineTransformMakeScale(0, 0);
+        self.pinHoleView.transform = CGAffineTransformMakeScale(0, 0);
     }completion:^(BOOL finished) {
+        
         completion();
     }];
 }
 
 - (void)contractView
 {
-    CGFloat endOriginY = self.hogeView.frame.origin.y + self.hogeView.frame.size.height;
+    CGFloat endOriginY = self.pinView.frame.origin.y + self.pinView.frame.size.height;
     
     CGRect dotEndRect = self.dotView.frame;
     dotEndRect.origin.y = endOriginY - dotEndRect.size.height;
     self.dotView.frame = dotEndRect;
     
-    CGRect pinRect = self.hogeView.frame;
+    CGRect pinRect = self.pinView.frame;
     pinRect.origin.y = endOriginY;
-    self.hogeView.frame = pinRect;
+    pinRect.size.height = 0.0f;
+    self.pinView.frame = pinRect;
 }
 
 @end
