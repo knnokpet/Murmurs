9589bb500173ff725de3fb529a73fc7055d69dd9
pr-field <knnokpet@gmail.com>
2014-09-04 23:21:32 +0900

    コードの追加。表示の修正。
    commonInit メソッドを追加。
    吹き出しの色を変更。
    吹き出しの描画方法を変更。UIBezeirPath を基にパスを描く様に。

--- MBTwitter/MBMessageView.m
+++ MBTwitter/MBMessageView.m
@@ -11,8 +11,7 @@
 @implementation MBMessageView
 - (void)awakeFromNib
 {
-    [self setPopsFromRight:NO];
-    self.contentMode = UIViewContentModeRedraw;
+    [self commonInit];
 }
 
 - (id)initWithFrame:(CGRect)frame
@@ -20,8 +19,16 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+        [self commonInit];
     }
     return self;
+}
+
+- (void)commonInit
+{
+    [self setPopsFromRight:NO];
+    self.contentMode = UIViewContentModeRedraw;
+    self.backgroundColor = [UIColor clearColor];
 }
 
 - (void)setPopsFromRight:(BOOL)popsFromRight
@@ -43,84 +50,76 @@
 - (void)drawPopInContext:(CGContextRef )context
 {
     CGFloat r, g, b, a;
-    r = 0.9;
-    g = 0.9;
-    b = 0.9;
+    r = 0.92;
+    g = 0.92;
+    b = 0.90;
     a = 1.0;
     if (self.popsFromRight) {
-        r = 0;
-        g = 0.4;
-        b = 0.8;
+        r = 0.17;
+        g = 0.58;
+        b = 1.0;
     }
-    CGFloat components[4] = {r, g, b, a};
-    CGContextSetStrokeColor(context, components);
-    CGContextSetFillColor(context, components);
+    
+    CGFloat strokeR, strokeG, strokeB, strokeA;
+    strokeA = 1.0;
+    if (self.popsFromRight) {
+        strokeR = strokeG = strokeB = 0.85;
+    }
+    CGFloat strokeWidth = 1.0;
     
     CGRect bouds = self.bounds;
     CGFloat spaceWidth = 8.0f;
-    CGFloat radius = 12.0f;
+    CGFloat radius = 16.0f;
     
     
     
     CGPoint popPoint = CGPointZero;
     
-    // roundRectPoints
-    CGFloat minX, nealyX, maxX, farX;
-    CGFloat minY, nealyY, maxY, farY;
     
+    UIBezierPath *bezierPath;
     if (self.popsFromRight) {
         popPoint = CGPointMake(CGRectGetMaxX(bouds), CGRectGetMaxY(bouds));
+        CGRect bezierRect = CGRectMake(CGRectGetMinX(bouds), CGRectGetMinY(bouds), CGRectGetWidth(bouds) - spaceWidth, CGRectGetHeight(bouds));
+        bezierPath = [UIBezierPath bezierPathWithRoundedRect:bezierRect cornerRadius:radius];
+        [bezierPath addLineToPoint:CGPointMake(bezierRect.size.width, bezierRect.size.height - radius)];
+        CGPoint controlPoint = CGPointMake(bezierRect.size.width, bezierRect.size.height);
+        [bezierPath addQuadCurveToPoint:popPoint controlPoint:controlPoint];
+        [bezierPath addQuadCurveToPoint:CGPointMake(bezierRect.size.width - radius, bezierRect.size.height - radius / 2) controlPoint:controlPoint];
+
+        [[UIColor colorWithRed:r green:g blue:b alpha:a] setFill];
         
-        minX = CGRectGetMinX(bouds);
-        nealyX = radius;
-        maxX = CGRectGetMaxX(bouds) - spaceWidth;
-        farX = maxX - radius;
-        minY = CGRectGetMinY(bouds);
-        nealyY = radius;
-        maxY = CGRectGetMaxY(bouds);
-        farY = maxY - radius;
-        
-        CGContextMoveToPoint(context, minX, nealyY);
-        CGContextAddArcToPoint(context, minX, minY, nealyX, minY, radius);
-        CGContextAddLineToPoint(context, farX, minY);
-        CGContextAddArcToPoint(context, maxX, minY, maxX, nealyY, radius);
-        CGContextAddLineToPoint(context, maxX, farY);
-        
-        CGContextAddArcToPoint(context, maxX, maxY, popPoint.x, popPoint.y, 12.0f);
-        CGContextAddLineToPoint(context, nealyX, maxY);
-        CGContextAddArcToPoint(context, minX, maxY, minX, farY, radius);
-        CGContextAddLineToPoint(context, minX, nealyY);
+        CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
+        UIColor *gradientStartColor = [UIColor colorWithRed:r green:g blue:b alpha:a];
+        UIColor *gradientEndColor = [UIColor colorWithRed:0.1 green:0.55 blue:1.0 alpha:a];
+        NSArray *gradientColors = @[(id)gradientStartColor.CGColor, (id)gradientEndColor.CGColor];
+        CGFloat gradientLocations[] = {0, 1};
+        CGGradientRef gradient = CGGradientCreateWithColors(colorSpaceRef, (CFArrayRef)gradientColors, gradientLocations);
+        CGContextSaveGState(context);
+        [bezierPath fill];
+        [bezierPath addClip];
+        CGContextDrawLinearGradient(context, gradient, CGPointMake(0, 0), CGPointMake(0, CGRectGetHeight(bouds)), 0);
+        CGColorSpaceRelease(colorSpaceRef);
+        CGGradientRelease(gradient);
+        CGContextRestoreGState(context);
         
     } else {
         // Fukidashi
         popPoint = CGPointMake(CGRectGetMinX(bouds), CGRectGetMaxY(bouds));
         
-        // roundRectPoints
-        minX = CGRectGetMinX(bouds) + spaceWidth;
-        nealyX = radius;
-        maxX = CGRectGetMaxX(bouds);
-        farX = maxX - radius;
-        minY = CGRectGetMinY(bouds);
-        nealyY = radius;
-        maxY = CGRectGetMaxY(bouds);
-        farY = maxY - radius;
+        CGRect bezierRect = CGRectMake(CGRectGetMinX(bouds) + spaceWidth, CGRectGetMinY(bouds), CGRectGetWidth(bouds) - spaceWidth, CGRectGetHeight(bouds));
+        bezierPath = [UIBezierPath bezierPathWithRoundedRect:bezierRect cornerRadius:radius];
+        [bezierPath addLineToPoint:CGPointMake(bezierRect.origin.x + radius, bezierRect.size.height - radius / 2)];
+        CGPoint controlPoint = CGPointMake(spaceWidth, CGRectGetMaxY(bouds));
+        [bezierPath addQuadCurveToPoint:popPoint controlPoint:controlPoint];
+        [bezierPath addQuadCurveToPoint:CGPointMake(bezierRect.origin.x, bezierRect.size.height - radius) controlPoint:controlPoint];
         
-        CGContextMoveToPoint(context, minX, nealyY);
-        CGContextAddArcToPoint(context, minX, minY, nealyX, minY, radius);
-        CGContextAddLineToPoint(context, farX, minY);
-        CGContextAddArcToPoint(context, maxX, minY, maxX, nealyY, radius);
-        CGContextAddLineToPoint(context, maxX, farY);
-        CGContextAddArcToPoint(context, maxX, maxY, farX, maxY, radius);
-        
-        CGContextAddLineToPoint(context, popPoint.x, popPoint.y);
-        CGContextAddArcToPoint(context, minX, maxY, minX, maxY - spaceWidth, 12.0f);
-        CGContextAddLineToPoint(context, minX, nealyY);
+        [[UIColor colorWithRed:r green:g blue:b alpha:a] setFill];
+        [[UIColor colorWithRed:strokeR green:strokeG blue:strokeB alpha:strokeA] setStroke];
+        bezierPath.lineWidth = strokeWidth;
+        //[bezierPath stroke];
+        [bezierPath fill];
     }
     
-    
-    
-    CGContextClosePath(context);
-    CGContextDrawPath(context, kCGPathFillStroke);
 }
 
 @end
