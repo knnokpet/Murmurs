43bb413e02c9d2a85dee2efcc3f2bac493622361
pr-field <knnokpet@gmail.com>
2014-09-12 10:38:29 +0900

    ボタンが押された際の境界線の色を変更するための実装を追加。
    drawRect メソッドに背景色を変えるメソッドを追加。
    タッチイベントを検出して、ボタンの状態を変えるメソッドを追加。
    境界線の色を変更するために、ボタンの状態を伝えるデリゲートメソッドを追加。

--- MBTwitter/MBTimelineActionButton.h
+++ MBTwitter/MBTimelineActionButton.h
@@ -8,11 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol MBTimelineActionButtonDelegate;
 @interface MBTimelineActionButton : UIButton
 
+@property (nonatomic, weak) id <MBTimelineActionButtonDelegate> delegate;
+
+@property (nonatomic) NSUInteger index;
 @property (nonatomic, readonly) NSString *title;
 @property (nonatomic, readonly) UIImage *image;
 
 - (instancetype)initWithTitle:(NSString *)title image:(UIImage *)image;
+
+@end
+
+
+@protocol MBTimelineActionButtonDelegate <NSObject>
+
+- (void)timelineActionButton:(MBTimelineActionButton *)control isSelected:(BOOL)selected;
 
 @end
--- MBTwitter/MBTimelineActionButton.m
+++ MBTwitter/MBTimelineActionButton.m
@@ -8,6 +8,13 @@
 
 #import "MBTimelineActionButton.h"
 
+@interface MBTimelineActionButton()
+
+@property (nonatomic, readonly) UIColor *defaultBackgroundColor;
+@property (nonatomic, readonly) UIColor *selectedBackgroundColor;
+
+@end
+
 @implementation MBTimelineActionButton
 
 - (id)initWithFrame:(CGRect)frame
@@ -26,7 +33,9 @@
         _title = title;
         _image = image;
         self.frame = CGRectMake(0, 0, 80, 44);
-        self.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.8];
+        _defaultBackgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.9];
+        _selectedBackgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5];
+        
         self.titleLabel.textAlignment = NSTextAlignmentCenter;
         self.titleLabel.font = [UIFont systemFontOfSize:14.0f];
         
@@ -36,25 +45,28 @@
         [self setImage:self.image forState:UIControlStateNormal];
         
         // highlighte state
-        [self setTitle:self.title forState:UIControlStateHighlighted];
-        [self setTitleColor:[UIColor whiteColor] forState:UIControlStateHighlighted];
-        [self setImage:self.image forState:UIControlStateHighlighted];
+        [self setTitle:self.title forState:UIControlStateSelected | UIControlStateHighlighted];
+        [self setTitleColor:[UIColor whiteColor] forState:UIControlStateSelected | UIControlStateHighlighted];
+        [self setImage:self.image forState:UIControlStateSelected | UIControlStateHighlighted];
         
         // disabled state
         [self setTitleColor:[UIColor blackColor] forState:UIControlStateDisabled];
         
+        [self addTarget:self action:@selector(touchBegan:withEvent:) forControlEvents:UIControlEventTouchDown];
+        [self addTarget:self action:@selector(touchMoving:withEvent:) forControlEvents:UIControlEventTouchDragInside | UIControlEventTouchDragOutside];
+        [self addTarget:self action:@selector(touchCanceled:withEvent:) forControlEvents:UIControlEventTouchDragOutside | UIControlEventTouchDragOutside];
+        [self addTarget:self action:@selector(touchEnded:withEvent:) forControlEvents:UIControlEventTouchUpInside];
     }
     return self;
 }
 
-- (void)setHighlighted:(BOOL)highlighted
+- (void)setSelected:(BOOL)selected
 {
-    [super setHighlighted:highlighted];
+    [super setSelected:selected];
+    [self setNeedsDisplay];
     
-    if (highlighted) {
-        self.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5];
-    } else {
-        self.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.8];
+    if ([_delegate respondsToSelector:@selector(timelineActionButton:isSelected:)]) {
+        [_delegate timelineActionButton:self isSelected:selected];
     }
 }
 
@@ -75,40 +87,51 @@
 
 }
 
-/*
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
+    
     [super drawRect:rect];
     // Drawing code
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGColorRef filledColorRef;
     
-    [[UIColor blackColor] setStroke];
     if (self.selected) {
-        [[UIColor clearColor] setFill];
+        filledColorRef = CGColorCreateCopy(self.selectedBackgroundColor.CGColor);
+        
+    } else {
+        filledColorRef = CGColorCreateCopy(self.defaultBackgroundColor.CGColor);
     }
     
-    CGRect bounds = self.bounds;
-    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(bounds.origin.x + bounds.size.width / 2, bounds.origin.y + bounds.size.height, bounds.size.width, bounds.size.height)];
-    [bezierPath stroke];
-    [bezierPath fill];
-}*/
-/* ボタン内で弄るとボタン特有の動きがなくなる*
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
-{
-    UITouch *touch = [touches anyObject];
-    
-    self.backgroundColor = [UIColor clearColor];
-}
-
-- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
-{
+    CGContextSetFillColorWithColor(context, filledColorRef);
+    CGContextFillRect(context, rect);
+    CGColorRelease(filledColorRef);
     
 }
 
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+- (void)touchBegan:(UIButton *)control withEvent:(UIEvent *)event
 {
+    self.selected = YES;
     self.highlighted = YES;
-}*/
+}
+
+- (void)touchMoving:(UIButton *)control withEvent:(UIEvent *)event
+{
+    self.selected = YES;
+    self.highlighted = YES;
+}
+
+- (void)touchCanceled:(UIButton *)control withEvent:(UIEvent *)event
+{
+    self.selected = NO;
+    self.highlighted = NO;
+}
+
+- (void)touchEnded:(UIButton *)control withEvent:(UIEvent *)event
+{
+    self.selected = YES;
+}
 
 @end
