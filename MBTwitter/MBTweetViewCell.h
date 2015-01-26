b7e04f46ccdebe8bb2d23b389034eb7ac682f0c8
knnokpet <knnokpet@gmail.com>
2015-01-26 15:47:54 +0900

    Modified composition.
    Change composition to layoutSubViews base from xib file base.

--- MBTwitter/MBTweetViewCell.h
+++ MBTwitter/MBTweetViewCell.h
@@ -8,13 +8,13 @@
 
 #import <UIKit/UIKit.h>
 #import "MBAvatorImageView.h"
-#import "MBTimelineImageContainerView.h"
 
 @class MBTweetTextView;
 @class MBCharacterScreenNameView;
 @class MBRetweetView;
 @class MBFavoriteView;
 @class MBPlaceWithGeoIconView;
+@class MBMediaImageView;
 @protocol MBtweetViewCellLongPressDelegate;
 @interface MBTweetViewCell : UITableViewCell
 
@@ -22,17 +22,20 @@
 @property (nonatomic, weak) id <MBtweetViewCellLongPressDelegate> delegate;
 
 
-@property (weak, nonatomic) IBOutlet MBCharacterScreenNameView *characterScreenNameView;
-@property (weak, nonatomic) IBOutlet MBRetweetView *retweeterView;
-@property (weak, nonatomic) IBOutlet MBPlaceWithGeoIconView *placeNameView;
+@property (nonatomic) MBCharacterScreenNameView *characterScreenNameView;
+@property (nonatomic) MBTweetTextView *dateView;
+@property (nonatomic) MBTweetTextView *tweetTextView;
+@property (nonatomic) MBAvatorImageView *avatorImageView;
+@property (nonatomic) MBFavoriteView *favoriteView;
+@property (nonatomic) MBMediaImageView *mediaImageView;
+@property (nonatomic) MBRetweetView *retweeterView;
+@property (nonatomic) MBPlaceWithGeoIconView *placeNameView;
 
-@property (weak, nonatomic) IBOutlet MBTweetTextView *dateView;
-@property (weak, nonatomic) IBOutlet MBTweetTextView *tweetTextView;
-@property (weak, nonatomic) IBOutlet MBAvatorImageView *avatorImageView;
-@property (weak, nonatomic) IBOutlet MBFavoriteView *favoriteView;
-@property (weak, nonatomic) IBOutlet MBTimelineImageContainerView *imageContainerView;
 
-
+@property (nonatomic, readonly, assign) BOOL requireFavorite;
+@property (nonatomic, readonly, assign) BOOL requirePlace;
+@property (nonatomic, readonly, assign) BOOL requireRetweet;
+@property (nonatomic, readonly, assign) BOOL requireMediaImage;
 
 @property (nonatomic) NSNumber *userID;
 @property (nonatomic) NSString *userIDStr;
@@ -40,14 +43,18 @@
 @property (nonatomic, readonly) NSAttributedString *charaScreenString;
 @property (nonatomic, readonly) NSAttributedString *placeString;
 
+
++ (CGFloat)heightForCellWithTweetText:(NSAttributedString *)tweetText constraintSize:(CGSize)constraintSize lineSpace:(CGFloat)lineSpace paragraphSpace:(CGFloat)paragraphSpace font:(UIFont *)font isRetweet:(BOOL)isRetweet isPlace:(BOOL)isPlace isMedia:(BOOL)isMedia;
+- (void)commonLayouts;
+
 - (void)addAvatorImage:(UIImage *)image;
 - (CGSize)avatorImageViewSize;
 - (CGFloat)avatorImageViewRadius;
 
-- (void)removeRetweetView;
-- (void)removePlaceNameView;
-- (void)removeFavoriteView;
-- (void)removeImageContainerView;
+- (void)setRequireFavorite:(BOOL)requireFavorite;
+- (void)setRequirePlace:(BOOL)requirePlace;
+- (void)setRequireRetweet:(BOOL)requireRetweet;
+- (void)setRequireMediaImage:(BOOL)requireMediaImage;
 
 - (void)setDateString:(NSAttributedString *)dateString;
 - (void)setCharaScreenString:(NSAttributedString *)charaScreenString;
--- MBTwitter/MBTweetViewCell.m
+++ MBTwitter/MBTweetViewCell.m
@@ -7,6 +7,7 @@
 //
 
 #import "MBTweetViewCell.h"
+#import <QuartzCore/QuartzCore.h>
 
 #import "MBCharacterScreenNameView.h"
 #import "MBTweetTextView.h"
@@ -14,7 +15,89 @@
 #import "MBFavoriteView.h"
 #import "MBPlaceWithGeoIconView.h"
 
+#import "MBMediaImageView.h"
+
+const CGFloat avatorImageViewWidthHeight = 48;
+const CGFloat attachedViewHeight = 18;
+
+const CGFloat mediaImageHeight = 128;
+const CGFloat dateViewWidth = 40;
+const CGFloat favoriteViewWidthHeight = 20;
+
+struct Layouts {
+    NSInteger top;
+    NSInteger left;
+    NSInteger right;
+    NSInteger bottom;
+};
+
+@interface MBTweetViewCell()
+{
+    struct Layouts avatorImageViewLayouts;
+    struct Layouts characterScreenNameViewLayouts;
+    struct Layouts dateViewLayouts;
+    struct Layouts tweetTextViewLayouts;
+    struct Layouts favoriteViewLayouts;
+    struct Layouts mediaImageContainerViewLayouts;
+    struct Layouts retweetViewLayouts;
+    struct Layouts placeNameViewLayouts;
+}
+
+@end
+
 @implementation MBTweetViewCell
+#pragma mark -
+#pragma mark Class Method
++ (CGFloat)heightForCellWithTweetText:(NSAttributedString *)tweetText constraintSize:(CGSize)constraintSize lineSpace:(CGFloat)lineSpace paragraphSpace:(CGFloat)paragraphSpace font:(UIFont *)font isRetweet:(BOOL)isRetweet isPlace:(BOOL)isPlace isMedia:(BOOL)isMedia
+{
+    MBTweetViewCell *cell = [[MBTweetViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+    [cell commonLayouts];
+    
+    return [cell heightForCellWithTweetText:tweetText constraintSize:constraintSize lineSpace:lineSpace paragraphSpace:paragraphSpace font:font isRetweet:isRetweet isPlace:isPlace isMedia:isMedia];
+}
+
+- (CGFloat)heightForCellWithTweetText:(NSAttributedString *)tweetText constraintSize:(CGSize)constraintSize lineSpace:(CGFloat)lineSpace paragraphSpace:(CGFloat)paragraphSpace font:(UIFont *)font isRetweet:(BOOL)isRetweet isPlace:(BOOL)isPlace isMedia:(BOOL)isMedia
+{
+    CGFloat minimumHeight = avatorImageViewWidthHeight + avatorImageViewLayouts.top * 2;
+    
+    CGFloat tweetViewMargin = characterScreenNameViewLayouts.top + attachedViewHeight + characterScreenNameViewLayouts.bottom;
+    
+    NSInteger tweetTextWidth = [self tweetTextWidthConstraintWidth:constraintSize.width];
+    CGRect textRect = [MBTweetTextView frameRectWithAttributedString:tweetText constraintSize:CGSizeMake(tweetTextWidth, CGFLOAT_MAX) lineSpace:lineSpace paragraghSpace:paragraphSpace font:font];
+    
+    CGFloat bottomHeight = tweetTextViewLayouts.bottom;
+    if (isRetweet && isPlace && isMedia) {
+        bottomHeight = (mediaImageContainerViewLayouts.top + mediaImageHeight) + (placeNameViewLayouts.top + attachedViewHeight + placeNameViewLayouts.bottom) + (attachedViewHeight + retweetViewLayouts.bottom);
+        
+    } else if (isRetweet && isPlace) {
+        bottomHeight = (placeNameViewLayouts.top + attachedViewHeight + placeNameViewLayouts.bottom) + (attachedViewHeight + retweetViewLayouts.bottom);
+        
+    } else if (isRetweet && isMedia) {
+        bottomHeight = (mediaImageContainerViewLayouts.top + mediaImageHeight + mediaImageContainerViewLayouts.bottom) + (attachedViewHeight + retweetViewLayouts.bottom);
+        
+    } else if (isPlace && isMedia) {
+        bottomHeight = (mediaImageContainerViewLayouts.top + mediaImageHeight) + (placeNameViewLayouts.top + attachedViewHeight + placeNameViewLayouts.bottom);
+        
+    } else if (isRetweet) {
+        bottomHeight = (retweetViewLayouts.top + attachedViewHeight + retweetViewLayouts.bottom);
+        
+    } else if (isPlace) {
+        bottomHeight = (placeNameViewLayouts.top + attachedViewHeight + placeNameViewLayouts.bottom);
+        
+    } else if (isMedia) {
+        bottomHeight = (mediaImageContainerViewLayouts.top + mediaImageHeight + mediaImageContainerViewLayouts.bottom);
+    }
+    
+    CGFloat customCellHeight = textRect.size.height + tweetViewMargin + bottomHeight;
+    
+    return  MAX(minimumHeight, customCellHeight);
+}
+
+- (CGFloat)tweetTextWidthConstraintWidth:(CGFloat)constraintWidth
+{
+    return constraintWidth - (tweetTextViewLayouts.left + tweetTextViewLayouts.right);
+}
+
 #pragma mark -
 #pragma mark Initialize
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
@@ -22,6 +105,9 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         // Initialization code
+        [self commonLayouts];
+        [self configureViews];
+        [self common];
     }
     return self;
 }
@@ -40,13 +126,69 @@
 
 - (void)common
 {
-    self.avatorImageView.backgroundColor = [UIColor clearColor];
     self.avatorImageView.layer.cornerRadius = 4.0f;
     self.avatorImageView.layer.masksToBounds = NO;
     self.avatorImageView.layer.shouldRasterize = YES;
+    self.avatorImageView.userInteractionEnabled = YES;
     
     _longPressRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(didLongPress)];
     [self addGestureRecognizer:self.longPressRecognizer];
+}
+
+- (void)commonLayouts
+{
+    avatorImageViewLayouts.left = 8;
+    avatorImageViewLayouts.top = 12;
+    avatorImageViewLayouts.right = characterScreenNameViewLayouts.left = 8;
+    characterScreenNameViewLayouts.top = 10;
+    characterScreenNameViewLayouts.right = dateViewLayouts.left = 10;
+    characterScreenNameViewLayouts.bottom = tweetTextViewLayouts.top = 2;
+    dateViewLayouts.top = 12;
+    dateViewLayouts.right = 8;
+    favoriteViewLayouts.left = 4;
+    favoriteViewLayouts.top = 10;
+    favoriteViewLayouts.right = 4;
+    tweetTextViewLayouts.left = 64;
+    tweetTextViewLayouts.right = 8;
+    tweetTextViewLayouts.bottom = 10;
+    mediaImageContainerViewLayouts.left = 64;
+    mediaImageContainerViewLayouts.right = 8;
+    mediaImageContainerViewLayouts.top = 8;
+    mediaImageContainerViewLayouts.bottom = 10;
+    placeNameViewLayouts.left = 32;
+    placeNameViewLayouts.right = 8;
+    placeNameViewLayouts.top = 4;
+    placeNameViewLayouts.bottom = 10;
+    retweetViewLayouts.left = 32;
+    retweetViewLayouts.right = 8;
+    retweetViewLayouts.top = 4;
+    retweetViewLayouts.bottom = 4;
+}
+
+- (void)configureViews
+{
+    CGSize avatorImageViewSize = CGSizeMake(avatorImageViewWidthHeight, avatorImageViewWidthHeight);
+    self.avatorImageView = [[MBAvatorImageView alloc] initWithFrame:CGRectMake(avatorImageViewLayouts.left, avatorImageViewLayouts.top, avatorImageViewSize.width, avatorImageViewSize.height)];
+    [self addSubview:self.avatorImageView];
+    
+    self.characterScreenNameView = [[MBCharacterScreenNameView alloc] initWithFrame:CGRectMake(avatorImageViewLayouts.left + avatorImageViewSize.width + characterScreenNameViewLayouts.left, characterScreenNameViewLayouts.top, 0, attachedViewHeight)];
+    self.characterScreenNameView.backgroundColor = [UIColor whiteColor];
+    [self addSubview:self.characterScreenNameView];
+    self.tweetTextView = [[MBTweetTextView alloc] initWithFrame:CGRectMake(tweetTextViewLayouts.left, self.characterScreenNameView.frame.origin.y + self.characterScreenNameView.bounds.size.height + tweetTextViewLayouts.top, 0, 0)];
+    self.tweetTextView.backgroundColor = [UIColor whiteColor];
+    [self addSubview:self.tweetTextView];
+    self.dateView = [[MBTweetTextView alloc] initWithFrame:CGRectMake(0, dateViewLayouts.top, dateViewWidth, attachedViewHeight)];
+    self.dateView.backgroundColor = [UIColor whiteColor];
+    self.dateView.alignment = NSTextAlignmentRight;
+    [self addSubview:self.dateView];
+    
+    self.favoriteView = [[MBFavoriteView alloc] initWithFrame:CGRectMake(0, favoriteViewLayouts.top, favoriteViewWidthHeight, favoriteViewWidthHeight)];
+    self.mediaImageView = [[MBMediaImageView alloc] initWithFrame:CGRectMake(mediaImageContainerViewLayouts.left, 0, 0, mediaImageHeight)];
+    self.placeNameView = [[MBPlaceWithGeoIconView alloc] initWithFrame:CGRectMake(placeNameViewLayouts.left, 0, 0, attachedViewHeight)];
+    self.placeNameView.backgroundColor = [UIColor whiteColor];
+    self.retweeterView = [[MBRetweetView alloc] initWithFrame:CGRectMake(retweetViewLayouts.left, 0, 0, attachedViewHeight)];
+    self.retweeterView.backgroundColor = [UIColor whiteColor];
+    
 }
 
 #pragma mark -
@@ -56,6 +198,50 @@
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+- (void)setRequireFavorite:(BOOL)requireFavorite
+{
+    _requireFavorite = requireFavorite;
+    if (requireFavorite && !self.favoriteView.superview) {
+        [self addSubview:self.favoriteView];
+    } else if (!requireFavorite && self.favoriteView.superview) {
+        [self.favoriteView removeFromSuperview];
+    }
+    [self setNeedsLayout];
+}
+
+- (void)setRequirePlace:(BOOL)requirePlace
+{
+    _requirePlace = requirePlace;
+    if (requirePlace && !self.placeNameView.superview) {
+        [self addSubview:self.placeNameView];
+    } else if (!requirePlace && self.placeNameView.superview) {
+        [self.placeNameView removeFromSuperview];
+    }
+    [self setNeedsLayout];
+}
+
+- (void)setRequireRetweet:(BOOL)requireRetweet
+{
+    _requireRetweet = requireRetweet;
+    if (requireRetweet && !self.retweeterView.superview) {
+        [self addSubview:self.retweeterView];
+    } else if (!requireRetweet && self.retweeterView.superview) {
+        [self.retweeterView removeFromSuperview];
+    }
+    [self setNeedsLayout];
+}
+
+- (void)setRequireMediaImage:(BOOL)requireMediaImage
+{
+    _requireMediaImage = requireMediaImage;
+    if (requireMediaImage && !self.mediaImageView.superview) {
+        [self addSubview:self.mediaImageView];
+    } else if (!requireMediaImage && self.mediaImageView.superview) {
+        [self.mediaImageView removeFromSuperview];
+    }
+    [self setNeedsLayout];
 }
 
 - (void)setUserIDStr:(NSString *)userIDStr
@@ -113,38 +299,6 @@
     return radius;
 }
 
-- (void)removeRetweetView
-{
-    if (self.retweeterView.superview) {
-        [self.retweeterView removeFromSuperview];
-        self.retweeterView = nil;
-    }
-}
-
-- (void)removePlaceNameView
-{
-    if (self.placeNameView.superview) {
-        [self.placeNameView removeFromSuperview];
-        self.placeNameView = nil;
-    }
-}
-
-- (void)removeFavoriteView
-{
-    if (self.favoriteView.superview) {
-        [self.favoriteView removeFromSuperview];
-        self.favoriteView = nil;
-    }
-}
-
-- (void)removeImageContainerView
-{
-    if (self.imageContainerView.superview) {
-        [self.imageContainerView removeFromSuperview];
-        self.imageContainerView = nil;
-    }
-}
-
 - (void)didLongPress
 {
     CGPoint touchedPoint = [self.longPressRecognizer locationInView:self];
@@ -181,6 +335,120 @@
     
     return touchedView;
     
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    CGRect bounds = self.bounds;
+    
+    CGRect dateViewFrame = self.dateView.frame;
+    dateViewFrame.origin.x = bounds.size.width - (dateViewFrame.size.width + dateViewLayouts.right);
+    self.dateView.frame = dateViewFrame;
+    
+    CGRect characterScreenFrame = self.characterScreenNameView.frame;
+    characterScreenFrame.size.width = dateViewFrame.origin.x - (self.avatorImageView.frame.origin.x + self.avatorImageView.bounds.size.width + characterScreenNameViewLayouts.left);
+    self.characterScreenNameView.frame = characterScreenFrame;
+    
+    if (self.requireFavorite) {
+        CGRect favoriteFrame = self.favoriteView.frame;
+        favoriteFrame.origin.x = dateViewFrame.origin.x - (favoriteFrame.size.width + favoriteViewLayouts.right);
+        self.favoriteView.frame = favoriteFrame;
+        
+        CGRect characterNameFrame = self.characterScreenNameView.frame;
+        characterNameFrame.size.width = favoriteFrame.origin.x - (characterNameFrame.origin.x + favoriteViewLayouts.left);
+        self.characterScreenNameView.frame = characterNameFrame;
+    }
+    
+    NSInteger bottom = tweetTextViewLayouts.bottom;
+    if (self.requirePlace && self.requireRetweet && self.requireMediaImage) {
+        CGRect retweetFrame = self.retweeterView.frame;
+        retweetFrame.size.width = bounds.size.width - (retweetViewLayouts.right + retweetViewLayouts.left);
+        retweetFrame.origin.y = bounds.size.height - (attachedViewHeight + retweetViewLayouts.bottom);
+        self.retweeterView.frame = retweetFrame;
+        
+        CGRect placeFrame = self.placeNameView.frame;
+        placeFrame.size.width = bounds.size.width - (placeNameViewLayouts.right + placeNameViewLayouts.left);
+        placeFrame.origin.y = retweetFrame.origin.y - (attachedViewHeight + placeNameViewLayouts.bottom);
+        self.placeNameView.frame = placeFrame;
+        
+        CGRect containerFrame = self.mediaImageView.frame;
+        containerFrame.size.width = bounds.size.width - (mediaImageContainerViewLayouts.left + mediaImageContainerViewLayouts.right);
+        containerFrame.origin.y = placeFrame.origin.y - (mediaImageHeight + 4);
+        self.mediaImageView.frame = containerFrame;
+        
+        bottom = bounds.size.height - (self.mediaImageView.frame.origin.y + mediaImageContainerViewLayouts.top);
+        
+    } else if (self.requirePlace && self.requireRetweet) {
+        CGRect retweetFrame = self.retweeterView.frame;
+        retweetFrame.size.width = bounds.size.width - (retweetViewLayouts.right + retweetViewLayouts.left);
+        retweetFrame.origin.y = bounds.size.height - (attachedViewHeight + retweetViewLayouts.bottom);
+        self.retweeterView.frame = retweetFrame;
+        
+        CGRect placeFrame = self.placeNameView.frame;
+        placeFrame.size.width = bounds.size.width - (placeNameViewLayouts.right + placeNameViewLayouts.left);
+        placeFrame.origin.y = retweetFrame.origin.y - (attachedViewHeight + placeNameViewLayouts.bottom);
+        self.placeNameView.frame = placeFrame;
+        
+        bottom = bounds.size.height - (placeFrame.origin.y + placeNameViewLayouts.top);
+        
+    } else if (self.requireRetweet && self.requireMediaImage) {
+        CGRect retweetFrame = self.retweeterView.frame;
+        retweetFrame.size.width = bounds.size.width - (retweetViewLayouts.right + retweetViewLayouts.left);
+        retweetFrame.origin.y = bounds.size.height - (attachedViewHeight + retweetViewLayouts.bottom);
+        self.retweeterView.frame = retweetFrame;
+        
+        CGRect containerFrame = self.mediaImageView.frame;
+        containerFrame.size.width = bounds.size.width - (mediaImageContainerViewLayouts.left + mediaImageContainerViewLayouts.right);
+        containerFrame.origin.y = retweetFrame.origin.y - (mediaImageHeight + mediaImageContainerViewLayouts.bottom);
+        self.mediaImageView.frame = containerFrame;
+        
+        bottom = bounds.size.height - (self.mediaImageView.frame.origin.y + mediaImageContainerViewLayouts.top);
+        
+    } else if (self.requirePlace && self.requireMediaImage) {
+        
+        CGRect placeFrame = self.placeNameView.frame;
+        placeFrame.size.width = bounds.size.width - (placeNameViewLayouts.right + placeNameViewLayouts.left);
+        placeFrame.origin.y = bounds.size.height - (attachedViewHeight + placeNameViewLayouts.bottom);
+        self.placeNameView.frame = placeFrame;
+        
+        CGRect containerFrame = self.mediaImageView.frame;
+        containerFrame.size.width = bounds.size.width - (mediaImageContainerViewLayouts.left + mediaImageContainerViewLayouts.right);
+        containerFrame.origin.y = placeFrame.origin.y - (mediaImageHeight + 4);
+        self.mediaImageView.frame = containerFrame;
+        
+        bottom = bounds.size.height - (self.mediaImageView.frame.origin.y + mediaImageContainerViewLayouts.top);
+        
+    } else if (self.requirePlace) {
+        CGRect placeFrame = self.placeNameView.frame;
+        placeFrame.size.width = bounds.size.width - (placeNameViewLayouts.right + placeNameViewLayouts.left);
+        placeFrame.origin.y = bounds.size.height - (attachedViewHeight + placeNameViewLayouts.bottom);
+        self.placeNameView.frame = placeFrame;
+        
+        bottom = bounds.size.height - (self.placeNameView.frame.origin.y + placeNameViewLayouts.top);
+        
+    } else if (self.requireRetweet) {
+        CGRect retweetFrame = self.retweeterView.frame;
+        retweetFrame.size.width = bounds.size.width - (retweetViewLayouts.right + retweetViewLayouts.left);
+        retweetFrame.origin.y = bounds.size.height - (attachedViewHeight + retweetViewLayouts.bottom);
+        self.retweeterView.frame = retweetFrame;
+        
+        bottom = bounds.size.height - (self.retweeterView.frame.origin.y + retweetViewLayouts.top);
+    } else if (self.requireMediaImage) {
+        CGRect containerFrame = self.mediaImageView.frame;
+        containerFrame.size.width = bounds.size.width - (mediaImageContainerViewLayouts.left + mediaImageContainerViewLayouts.right);
+        containerFrame.origin.y = bounds.size.height - (mediaImageHeight + mediaImageContainerViewLayouts.bottom);
+        self.mediaImageView.frame = containerFrame;
+        
+        bottom = bounds.size.height - (self.mediaImageView.frame.origin.y + mediaImageContainerViewLayouts.top);
+        
+    }
+    
+    CGRect tweetFrame = self.tweetTextView.frame;
+    tweetFrame.size.width = [self tweetTextWidthConstraintWidth:bounds.size.width];
+    tweetFrame.size.height = bounds.size.height - (self.characterScreenNameView.frame.origin.y + self.characterScreenNameView.bounds.size.height + characterScreenNameViewLayouts.bottom + bottom);
+    self.tweetTextView.frame = tweetFrame;
 }
 
 @end
